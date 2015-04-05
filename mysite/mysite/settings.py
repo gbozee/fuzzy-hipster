d1b69fb22d26eb88393f84f7f85230714fd2432e
vagrant <vagrant@precise32.(none)>
2015-04-05 21:51:53 +0000

    changed database config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -58,9 +58,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mysite',
+        'USER': 'vagrant',
+        'PASSWORD': 'punnisher',
+        'HOST': '192.168.33.10',
+        'PORT': '5432',
     }
 }
 
